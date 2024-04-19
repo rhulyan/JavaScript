7984053dacb5417048014368f7b56de06e3f52fc
rhulyan <108284013+rhulyan@users.noreply.github.com>
2024-04-19 09:28:54 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -30,24 +30,29 @@ function calculaTempo(tempoObjetivo) {
     let minutos = Math.floor(segundos / 60);
     let horas = Math.floor(minutos / 60);
     let dias = Math.floor(horas / 24);
-    
+
     segundos %= 60;
     minutos %= 60;
     horas %= 24;
-    
-    return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
-}
-
-function atualizaCronometro(){
-    for (let i=0; i<contadores.length;i++){
-        contadores[i].textContent = calculaTempo(tempos[i]);   
+ 
+    if (tempoFinal > 0){
+        return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
+    } else {
+        return "Prazo Finalizado";
     }
-}
     
-    function comecaCronometro(){
+}
+ 
+function atualizaCronometro(){
+    for (let i = 0; i < contadores.length; i++) {
+        contadores[i].textContent = calculaTempo(tempos[i]);
+    }
+      }
+    
+      function comecaCronometro(){
         atualizaCronometro();
         setInterval(atualizaCronometro, 1000);
     }
     
-    comecaCronometro();
+//comecaCronometro();
     
